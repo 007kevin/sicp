fc72160dec6a937a8c85d43bba7b3c8b7585d9dc
kevin <gk@email.com>
2017-12-10 19:50:11 +0000

    done 1-19.scm

--- ch1/1-19.scm
+++ ch1/1-19.scm
@@ -1,2 +1,30 @@
 ;; Clever algorithm for computing Fibonacci numbers in a logarithmic
 ;; number of steps
+;;
+;; T0,1(a b) = (b + a, a)
+;;
+;; Tp,q(a b) = (bq + aq + ap, bp + aq)
+;;
+;; By applying the transformation twice...
+;;
+;; Tp,q(Tp,q(a b)) = (b(2pq+q^2) + a(2pq+q^2) + a(p^2+q^2),b(p^2+q^2) + a(2pq+q^2))
+;;
+;; is equivalent to the single transformation
+;;
+;; T p^2+q^2, 2pq+q^2 (a b)
+
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count) (fib-iter a
+                                 b
+                                 (+ (square p) (square q))
+                                 (+ (* 2 (* p q )) (square q))
+                                 (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
