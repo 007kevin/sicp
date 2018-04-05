cad68cbe3c3fd0b9664d4b95be0cd2e3246b6751
kevin <gk@email.com>
2018-04-05 01:17:06 +0000

    done excercise 1-28

--- ch1/1-28.scm
+++ ch1/1-28.scm
@@ -1,15 +1,19 @@
-(define (expmod base exp m)
-  (cond ((= exp 0) 1)
-        ((even? exp)
-         (remainder
-          (square (expmod base (/ exp 2) m))
-          m))
-        (else
-         (remainder
-          (* base (expmod base (- exp 1) m))
-          m))))
-
 (define (miller-rabin-test n)
+  (define (expmod base exp m)
+    (define (check val)
+      (cond ((= val 1) val)
+            ((= val (square (- n 1))) val)
+            ((= (remainder val n) 1) 0)
+            (else val)))
+    (cond ((= exp 0) 1)
+          ((even? exp)
+           (remainder
+            (check (square (expmod base (/ exp 2) m)))
+            m))
+          (else
+           (remainder
+            (* base (expmod base (- exp 1) m))
+            m))))
   (define (alt-fermat-test a)
     (cond ((= a 0) true)
           ((= (expmod a (- n 1) n) 1) (alt-fermat-test (- a 1)))
